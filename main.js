9870a4052deb2b4887151b2eec0dd8cebee87d41
Ratchet205 <205ratchet@gmail.com>
2023-12-15 07:38:08 +0100

    v0.1.0
    ground structure with selected color palette, starting it in full screen with tool and title bar hidden, hotkey for hidding and showing application via ctrl+shift+H. yet no way to close application (except for alt+f4) planning on adding ctrl+shift+Q for closing the app

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow, screen } = require('electron');
+const { globalShortcut } = require('electron/main');
+
+function createWindow() {
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+    const win = new BrowserWindow({
+        width: width,
+        height: height,
+        icon: 'public/img/ico/rplayer.ico',
+        fullscreen: true,
+        frame: false,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    win.loadFile('public/index.html')
+    globalShortcut.register('CommandOrControl+Shift+H', () => {
+        if (win) {
+          if (win.isVisible()) {
+            win.hide();
+          } else {
+            win.show();
+          }
+        }
+      });
+}
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
