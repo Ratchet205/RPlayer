4cae24675c44762123d2fc8aeafaa5dac75d2f9a
Ratchet205 <205ratchet@gmail.com>
2024-01-17 13:34:32 +0100

    regulated size/window handeling
    installed electron-store for saving window size from last use

--- main.js
+++ main.js
@@ -1,50 +1,90 @@
 const { app, BrowserWindow, screen } = require('electron');
 const { globalShortcut } = require('electron/main');
+const Store = require('electron-store');
+const store = new Store();
 
 function createWindow() {
-    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-    const win = new BrowserWindow({
-        width: width,
-        height: height,
-        icon: 'public/img/ico/rplayer.ico',
-        fullscreen: true,
-        frame: false,
-        autoHideMenuBar: true,
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    });
+  // Retrieve window state from the store or use default values
+  const windowState = store.get('windowState', {
+    width: screen.getPrimaryDisplay().workAreaSize.width,
+    height: screen.getPrimaryDisplay().workAreaSize.height,
+    frame: true,
+    autoHideMenuBar: true,
+    fullscreen: false,
+    maximized: false, // New property to remember maximized state
+  });
 
-    win.loadFile('public/index.html')
-    globalShortcut.register('CommandOrControl+Shift+H', () => {
-        if (win) {
-          if (win.isVisible()) {
-            win.hide();
-          } else {
-            win.show();
-          }
-        }
-      });
+  const win = new BrowserWindow({
+    width: windowState.width,
+    height: windowState.height,
+    fullscreen: false,
+    frame: windowState.frame,
+    autoHideMenuBar: windowState.autoHideMenuBar,
+    icon: 'public/img/ico/rplayer.ico',
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-    win.loadFile('public/index.html')
-    globalShortcut.register('CommandOrControl+Shift+Q', () => {
-        if (win) {
-          if (win.isVisible()) {
-            app.exit();
-          }
-        }
-      });
+  if (windowState.maximized) {
+    win.maximize();
+  }
+
+  win.loadFile('public/index.html');
+
+  // Register global shortcuts
+  globalShortcut.register('CommandOrControl+Shift+H', () => {
+    if (win) {
+      if (win.isVisible()) {
+        win.hide();
+      } else {
+        win.show();
+      }
+    }
+  });
+
+  globalShortcut.register('CommandOrControl+Shift+Q', () => {
+    if (win) {
+      if (win.isVisible()) {
+        app.exit();
+      }
+    }
+  });
+
+  // Save window state when the window is resized, moved, or maximized
+  win.on('resize', () => {
+    const { width, height } = win.getBounds();
+    store.set('windowState', { ...windowState, width, height, maximized: win.isMaximized() });
+  });
+
+  win.on('move', () => {
+    const { x, y } = win.getBounds();
+    store.set('windowState', { ...windowState, x, y, maximized: win.isMaximized() });
+  });
+
+  win.on('maximize', () => {
+    store.set('windowState', { ...windowState, maximized: true });
+  });
+
+  win.on('unmaximize', () => {
+    store.set('windowState', { ...windowState, maximized: false });
+  });
+
+  win.on('closed', () => {
+    app.quit(); // Quit the app when the window is closed
+  });
 }
+
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
